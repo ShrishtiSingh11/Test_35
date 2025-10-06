4f19a3237040dc78d48f4f0f9298650555efd5b3
Shrishti Singh <shristi010306@gmail.com>
2025-10-07 02:27:24 +0530

    Update and rename BSfirstOcc2D.java to TwenEightBSfirstOcc2D.java

--- TwenEightBSfirstOcc2D.java
+++ TwenEightBSfirstOcc2D.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class BSfirstOcc2D {
+public class TwenEightBSfirstOcc2D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
