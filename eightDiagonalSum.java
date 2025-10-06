35cec6bc1277a406238f38bdad73fca4e3137341
Shrishti Singh <shristi010306@gmail.com>
2025-10-07 01:39:38 +0530

    Update and rename diagonalSum.java to eightDiagonalSum.java

--- eightDiagonalSum.java
+++ eightDiagonalSum.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class diagonalSum{
+public class eightDiagonalSum{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int r=sc.nextInt();
