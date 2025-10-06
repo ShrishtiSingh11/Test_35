2cc8d520d8d2484cbac0697448c4cce0abd8e197
Shrishti Singh <shristi010306@gmail.com>
2025-10-07 02:09:06 +0530

    Create twenOneLowerBound.java

---
+++ twenOneLowerBound.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class twenOneLowerBound{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++) {
+          arr[i]=sc.nextInt();
+        }
+        int key=sc.nextInt();
+        int l=0,h=n-1,res=n;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(arr[m]>=key){
+              res=m;
+              h=m-1;
+            }
+            else l=m+1;
+        }
+        System.out.println(res);
+    }
+}
