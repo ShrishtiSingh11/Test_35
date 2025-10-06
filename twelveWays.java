a23ad186fdae89f9524c50f2c0248074348dfdfd
Shrishti Singh <shristi010306@gmail.com>
2025-10-07 01:48:33 +0530

    Create twelveWays.java

---
+++ twelveWays.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class twelveWays{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=1,b=1,c=0;
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        System.out.println(c);
+    }
+}
