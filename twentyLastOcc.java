5da814c56a9a2082070c42727368f74171e29231
Shrishti Singh <shristi010306@gmail.com>
2025-10-07 02:07:44 +0530

    Create twentyLastOcc.java

---
+++ twentyLastOcc.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class twentyLastOcc{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++) {
+          arr[i]=sc.nextInt();
+        }
+        int key=sc.nextInt();
+        int l=0,h=n-1,result=-1;
+        while(l<=h){
+            int m=l+(result-l)/2;
+            if(arr[m]==key)
+            {
+              result=m;
+              l=m+1;
+            }
+            else if(arr[m]<key){
+              l=m+1;
+            }
+            else {
+              r=m-1;
+            }
+        }
+        System.out.println(result);
+    }
+}
