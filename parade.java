2d96a1ca8427ef08887e2897d48bef07255a27be
Shrishti Singh <shristi010306@gmail.com>
2025-10-06 18:00:16 +0530

    Create parade.java
    Checking if the array is in non-decreasing order or not.

---
+++ parade.java
@@ -0,0 +1,20 @@
+public class parade{
+  public static void main(String[] args){
+  int []arr={1,2,3,6,9};
+    int low=0;
+    int high= arr.length-1;
+    boolean sorted=true;
+    while(low<high){
+    if(arr[low]>arr[low+1]){
+   sorted=false;
+      break;
+    }
+    low++;
+    }
+
+  if(sorted)
+     System.out.println("true");
+else
+       System.out.println("false");
+}
+}
